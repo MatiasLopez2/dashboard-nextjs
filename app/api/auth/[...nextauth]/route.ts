3ee6d34cd7f287c94ed32a24359dcf0887d2b8cc
MatiasLopez2 <matiaslopez0022@gmail.com>
2025-11-20 01:32:33 -0300

    actualizo route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,7 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "@/auth.config";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-});
+const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST };
