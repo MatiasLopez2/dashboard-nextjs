50f85b716430afadbc5fabb8bedddc30cafeb169
MatiasLopez2 <matiaslopez0022@gmail.com>
2025-11-20 15:46:16 -0300

    mejoras

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,7 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data'; // Remove fetchLatestInvoices
+import { fetchCardData } from '@/app/lib/data'; 
 import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
@@ -12,7 +12,6 @@ import {
 } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  // Remove `const latestInvoices = await fetchLatestInvoices()`
   const {
     numberOfInvoices,
     numberOfCustomers,
